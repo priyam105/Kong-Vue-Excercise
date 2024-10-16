ee8159fb2086e3ba5ac84bfe634ad2ef40fcda0c
priyam <priyam498@gmail.com>
2024-10-16 21:57:06 +0530

    test: added more unit tests for the components

---
+++ mocks/modalContent.ts
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: '01ed9e70-5d52-4350-8c40-713a8aa72f28',
+    type: 'REST',
+    versionName: '1.8.6',
+    versionDesc: 'Organized bifurcated hardware',
+    developerDetails: {
+      id: '1c18a98a-05bc-4111-96a8-5d7f8674f3ea',
+      name: 'Chester Little',
+      email: 'Chester_Little@hotmail.com',
+      avatar: 'https://avatars.githubusercontent.com/u/65182094',
+    },
+  },
+
+
+]
